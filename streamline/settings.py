f6689a4a11e2beb83da1a8a9cef6a5d34a540ee8
Anshu Adhikari <anshu.adhikari10@gmail.com>
2026-02-04 14:51:43 -0500

    add .env to gitignore

--- streamline/settings.py
+++ streamline/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(
+    BASE_DIR / '.env'
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -124,3 +128,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#airtable API Key
+AIRTABLE_API_KEY = os.getenv('AIRTABLE_API_KEY')
