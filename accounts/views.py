1e521cb12fdd1e92a275c60c83c68166ea00d0b6
Anshu Adhikari <anshu.adhikari10@gmail.com>
2026-02-04 15:05:25 -0500

    implement Airtable API integration in accounts app and update URL routing

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from pyairtable import Api
+from django.conf import settings
+from django.http import JsonResponse
 
-# Create your views here.
+api = Api(settings.AIRTABLE_API_KEY)
+
+
+def test(request):
+    table = api.table('appvOFmpLj8ZNNGPR', 'tbl25IPMcnCR5bOIR')  
+    response = table.all()
+    return JsonResponse(response, safe=False)
+
