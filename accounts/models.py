67f13383e89dcf977c7b41ed2d4bc88a15bc88b4
Anshu Adhikari <anshu.adhikari10@gmail.com>
2026-02-05 10:27:01 -0500

    implement custom user model with AirTableUser and associated manager in accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
+
+class AirTableUserManager(BaseUserManager):
+    def create_user(self, email, password=None, airtable_id=None):
+        """
+        Creates and saves a User with the given email, and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            airtable_id=airtable_id,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given emailand password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class AirTableUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    #cutomer id from airtable
+    airtable_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
+
+    objects = AirTableUserManager()
+
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return self.email
 
 # Create your models here.
--- streamline/settings.py
+++ streamline/settings.py
@@ -111,6 +111,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#Custom user model 
+AUTH_USER_MODEL = 'accounts.AirTableUser'
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
