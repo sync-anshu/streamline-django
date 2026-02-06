8621e7fe1ead638a7491dcc3fa90cebf71873637
Anshu Adhikari <anshu.adhikari10@gmail.com>
2026-02-06 10:02:45 -0500

    REST framework, adding authentication and returning Response instead of JsonResponse; enhance JWT authentication to handle token from cookies

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,18 @@
 from pyairtable import Api
 from django.conf import settings
 from django.http import JsonResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
 
 api = Api(settings.AIRTABLE_API_KEY)
 
-def test(request):
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def test(request: Request) -> Response:
     table = api.table('appvOFmpLj8ZNNGPR', 'tbl25IPMcnCR5bOIR')  
     response = table.all()
-    return JsonResponse(response, safe=False)
+    return Response(response, status=status.HTTP_200_OK)
 
--- api/authentication.py
+++ api/authentication.py
@@ -6,7 +6,18 @@ from rest_framework.request import Request
 
 class CustomJWTAuthentication(JWTAuthentication):
     def authenticate(self, request: Request):
-        return super().authenticate(request)
+        raw_token = request.COOKIES.get('access')
+        if raw_token is None:
+            return None
+        
+        try:
+            validated_token = self.get_validated_token(raw_token)
+            return self.get_user(validated_token), validated_token
+        except Exception as e:
+            return None
+
+
+
 
 
         
